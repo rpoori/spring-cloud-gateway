2b367ca6a19dc17ac609ae5161c836958be88870
Poornima Rajendhran <poornimarajendhran@Poornimas-Air.home>
2020-05-21 22:03:28 -0400

    Add application yml and hello world rest end point

--- app/src/main/java/com/my/poc/gateway/GatewayApplication.java
+++ app/src/main/java/com/my/poc/gateway/GatewayApplication.java
@@ -1,13 +1,32 @@
 package com.my.poc.gateway;
 
+import lombok.Builder;
+import lombok.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@GetMapping(value = "/api/hello")
+	public ResponseEntity<Message> helloWorld() {
+		return ResponseEntity.ok(Message.builder()
+				.msg("Hello World")
+				.build());
+	}
+
+}
+
+@Value
+@Builder
+class Message {
+	String msg;
 }
